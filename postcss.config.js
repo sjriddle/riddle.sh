aac9fcac16443594e89d9a5923a784254b78fb99
Riddle <samuel.riddle1@gmail.com>
2023-05-03 13:53:41 -0600

    Added PostCSS & Tailwind building configuration

---
+++ postcss.config.js
@@ -0,0 +1,17 @@
+const purgecss = require('@fullhuman/postcss-purgecss')({
+  content: [ './hugo_stats.json' ],
+  defaultExtractor: (content) => {
+    let els = JSON.parse(content).htmlElements;
+    return els.tags.concat(els.classes, els.ids);
+  }
+});
+
+module.exports = {
+  plugins: [
+    require("postcss-import"),
+    require("tailwindcss")( "assets/css/tailwind.config.js"),
+    require("autoprefixer"),
+    ...(process.env.HUGO_ENVIRONMENT === 'production' ? [ purgecss ] : [])
+
+  ],
+};
