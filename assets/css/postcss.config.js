3a4a0026e754ca1cbdaf236466fcc25090d78704
Riddle <samuel.riddle1@gmail.com>
2023-05-04 10:56:17 -0600

    implemented files into main project

---
+++ assets/css/postcss.config.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  plugins: [
+    require("postcss-import")({
+      path: ["assets/css"],
+    }),
+    require("tailwindcss")("assets/css/tailwind.config.js"),
+    require("autoprefixer")({
+      overrideBrowserslist: ['>1%']
+    }),
+  ],
+};
